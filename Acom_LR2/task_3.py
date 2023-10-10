8ea05d2903c46de8faa9b6c5fa3e267f5247e49e
shurasikk <90204027+shurasikk@users.noreply.github.com>
2023-10-10 21:09:34 +0300

    морфологические операции открытия и закрытия

--- Acom_LR2/task_3.py
+++ Acom_LR2/task_3.py
@@ -15,11 +15,17 @@ while True:
     mask = cv2.inRange(hsv, min_red, max_red)
     res = cv2.bitwise_and(frame, frame, mask=mask)
 
-    cv2.imshow('HSV_video', hsv)
-    cv2.imshow('Result_video', res)
+    kernel = np.ones((5, 5), np.uint8)
+    open = cv2.morphologyEx(res, cv2.MORPH_OPEN, kernel)
+    close = cv2.morphologyEx(res, cv2.MORPH_CLOSE, kernel)
 
+    cv2.imshow('Opening', open)
+    cv2.imshow('Closing', close)
+
+    # нажатие клавиши esc для выхода из цикла
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
+# освобождение ресурсов окна
 cap.release()
 cv2.destroyAllWindows()
